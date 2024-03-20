7c8102763e66930b9449e5db81ea4216747dddba
JOSE <jose@iMac-de-JOSE.local>
2024-03-20 09:12:26 +0100

    fetch terminado pero no bonito

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css';
-import { useStat, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
   const [pokemonName, setPokmeonName] = useState('');
-
-  function fetchPokemon(pokemon) {
-
-  }
+  const [pokemonData, setPokemonData] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     fetchPokemon(pokemonName)
+  };
+
+  const fetchPokemon = async (name) => {
+    try {
+      const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+      if (!response.ok) {
+        throw new Error('Pokemon not found!')
+      }
+      const data = await response.json();
+      setPokemonData(data);
+    } catch (error) {
+      console.error('Error fetching Pokemon:', error);
+      setPokemonData(null);
+    } 
   }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -21,9 +33,16 @@ function App() {
         <input type="text" id='nombre' name='nombre' value={pokemonName} onChange={(e) => setPokmeonName(e.target.value)} />
         <button type="submit">Buscar</button>
       </form>
+      <div className='pokemon-div'>
+        {pokemonData && (
+          <div>
+            <h2>{pokemonData.name}</h2>
+            <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+          </div>
+        )}
+      </div>
     </>
   )
 };
 
 export default App;
-
