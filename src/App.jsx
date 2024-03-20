cf2e4d4c8aa832fe9487c131ed4013e7684c01dc
JOSE <jose@iMac-de-JOSE.local>
2024-03-20 08:30:32 +0100

    inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import './App.css';
+import { useStat, useEffect, useState } from 'react';
 
-function App () {
-  <></>
+
+
+function App() {
+  const [pokemonName, setPokmeonName] = useState('');
+
+  function fetchPokemon(pokemon) {
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchPokemon(pokemonName)
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nombre">Nombre del Pokemon</label>
+        <input type="text" id='nombre' name='nombre' value={pokemonName} onChange={(e) => setPokmeonName(e.target.value)} />
+        <button type="submit">Buscar</button>
+      </form>
+    </>
+  )
 };
 
 export default App;
+
